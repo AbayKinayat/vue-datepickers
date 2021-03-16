f141238a5e9b20005bdb20530072013eb9c46503
Kinayat Abay <74435594+AbayKinayat@users.noreply.github.com>
2021-03-17 04:26:21 +0600

    vue-datepickers

---
+++ precache-manifest.975b5977bacb48b417dce44cbd5063f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75401a089c9b7a0d9f49",
+    "url": "/css/app.a5cb9354.css"
+  },
+  {
+    "revision": "90f56e75138405d5b0d3",
+    "url": "/css/chunk-vendors.358ba102.css"
+  },
+  {
+    "revision": "2e95bec427a9e500a7bfd02e5a6aed75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75401a089c9b7a0d9f49",
+    "url": "/js/app.2a3ff5ce.js"
+  },
+  {
+    "revision": "90f56e75138405d5b0d3",
+    "url": "/js/chunk-vendors.7c59a89a.js"
+  },
+  {
+    "revision": "ee6c4eef584777e634437c123c4d8d9d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
